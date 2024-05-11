31d9ee3d185f110e86dac56fc02ebf1d1b8b93a6
Nadar <git@nadar.io>
2024-05-11 19:55:23 +0000

    phpstan, phpcsfixer & rector

--- src/S3.php
+++ src/S3.php
@@ -2,14 +2,14 @@
 
 namespace indielab\yii2s3;
 
+use Aws\S3\S3Client;
 use yii\base\Component;
 use yii\base\InvalidConfigException;
 use yii\helpers\ArrayHelper;
-use Aws\S3\S3Client;
 
 /**
  * S3 Amazon Component.
- * 
+ *
  * @property \Aws\S3\S3Client $client The s3 client.
  * @author Basil Suter <basil@nadar.io>
  */
@@ -19,44 +19,44 @@ class S3 extends Component
      * @var string The region e.g `eu-central-1` which is EU Frankfurt.
      */
     public $region = null;
-    
+
     /**
      * @var string The name of the bucket.
      */
     public $bucket = null;
-    
+
     /**
      * @var string The aws key.
      */
     public $key = null;
-    
+
     /**
      * @var string The aws secret.
      */
     public $secret = null;
-    
+
     /**
      * @var string Access contorl: Valid values: private|public-read|public-read-write|authenticated-read|aws-exec-read|bucket-owner-read|bucket-owner-full-control
      */
     public $acl = 'public-read';
-    
+
     /**
      * @inheritdoc
      */
     public function init()
     {
         parent::init();
-        
+
         if ($this->region === null || $this->bucket === null || $this->key === null) {
             throw new InvalidConfigException("region, bucket and key must be provided for s3 component configuration.");
         }
     }
-    
+
     private $_client = null;
 
     /**
      * Get the Amazon client library.
-     * 
+     *
      * @return \Aws\S3\S3Client
      */
     public function getClient()
@@ -69,13 +69,13 @@ class S3 extends Component
             ]);
             $this->_client->registerStreamWrapper();
         }
-        
+
         return $this->_client;
     }
-    
+
     /**
      * Upload a file source to the Bucket.
-     * 
+     *
      * @param string $filePath
      * @param array $options You can provide options to the putObject method of the S3Client.
      * - override: Wehther to check if the file exists or not.
@@ -87,30 +87,30 @@ class S3 extends Component
     {
         $override = ArrayHelper::remove($options, 'override', false);
         $key = ArrayHelper::remove($options, 'Key', pathinfo($filePath, PATHINFO_BASENAME));
-        
+
         if (!$override && $this->find($key)) {
             return false;
         }
-        
+
         $configure = ArrayHelper::merge([
             'ACL' => $this->acl,
             'Bucket' => $this->bucket,
             'Key' => $key,
             'SourceFile' => $filePath,
         ], $options);
-        
+
         $put = $this->client->putObject($configure);
-        
+
         if ($put) {
             return $put['ObjectURL'];
         }
-        
+
         return false;
     }
-    
+
     /**
      * Get the url from a key file based on the bucket and region.
-     * 
+     *
      * @param string $key The key file.
      * @return string
      */
@@ -118,10 +118,10 @@ class S3 extends Component
     {
         return $this->client->getObjectUrl($this->bucket, $key);
     }
-    
+
     /**
      * See whether a key exists or not.
-     * 
+     *
      * @param string $key
      * @return \Aws\Result|boolean
      */
@@ -129,7 +129,7 @@ class S3 extends Component
     {
         try {
             return $this->client->getObject(['Bucket' => $this->bucket, 'Key' => $key]);
-        } catch (\Aws\S3\Exception\S3Exception $e) {
+        } catch (\Aws\S3\Exception\S3Exception) {
             return false;
         }
     }
@@ -144,7 +144,7 @@ class S3 extends Component
     {
         return fopen("s3://{$this->bucket}/{$key}", "r");
     }
-    
+
     /**
      * Delete a given file
      */
